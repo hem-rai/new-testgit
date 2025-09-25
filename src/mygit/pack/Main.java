26c833d5efb6332c2811a528221633817adc05a8
apple-picking <applesansar@gmail.com>
2025-09-25 16:26:33 -0400

    committed by apple-picking on Hem_develoop

--- src/mygit/pack/Main.java
+++ src/mygit/pack/Main.java
@@ -5,6 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello, this is the poject for GitHub");
 		System.out.println("Hello, this is Version 1 of the updated version of the git-project by apple-pick");
+	
+	System.out.println("This is updated virsion in Hem_develop branch");
+	
+	
 	}
 
 }
